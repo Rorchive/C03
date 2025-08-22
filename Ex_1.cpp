c9ddac8a6f59cddce91f43a2891ff00c3e689fb9
RodrigoCAndrade <198579161+RodrigoCAndrade@users.noreply.github.com>
2025-08-21 23:33:01 -0300

    🛠️ Improve Ex_1

--- Ex_1.cpp
+++ Ex_1.cpp
@@ -1,6 +1,28 @@
+/**
+ * @file: Ex_1.cpp
+ * @author: Rodrigo Andrade
+ * @date: 21 Aug 2025
+ * @license: MIT
+ * @language: C++
+ * @github: https://github.com/RodrigoCAndrade/C03
+ */
+
+
 #include <iostream>
 
 using namespace std;
+
+/**
+ * @brief: Searches for a number in an array.
+ * @return: The index of the found number or -1 if not found.
+ */
+int searchNumber(int numbers[], int size, int query) {
+    for (int i = 0; i < size; i++)
+        if (numbers[i] == query)
+            return i;
+
+    return -1;
+}
 
 int main() {
     int numbers[100]; // List of numbers
@@ -20,15 +42,8 @@ int main() {
     int query; // Number to search
     cin >> query;
 
-    int index = -1; // Index of the found number
-
     // Search for the number
-    for (int i = 0; i < size; i++) {
-        if (numbers[i] == query) {
-            index = i;
-            break;
-        }
-    }
+    int index = searchNumber(numbers, size, query);
 
     // Output the result
     if (index != -1) {
